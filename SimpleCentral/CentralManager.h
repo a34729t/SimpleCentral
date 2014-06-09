4b1a038f7d263f07afcedb30eb1b3d70f6467564
Nicolas Flacco <ncf1989@yahoo.com>
2014-06-09 14:23:31 -0700

    cleaned out stuff

--- SimpleCentral/CentralManager.h
+++ SimpleCentral/CentralManager.h
@@ -7,23 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-@import CoreBluetooth;
-
-@class CentralManager;
-
-@protocol CentralManagerDelegate <NSObject>
-
-- (void)didDiscoverPeripheral:(CBPeripheral *)peripheral rssi:(NSNumber *)RSSI;
-- (void)receivedFromPerpiheral:(CBPeripheral *)peripheral msg:(NSString *)msg;
-
-@end
 
 @interface CentralManager : NSObject
-
-@property(nonatomic) id <CentralManagerDelegate> delegate;
-
-+ (CentralManager*)sharedInstance;
-- (void)startScanning;
-- (void)stopScanning;
 
 @end
